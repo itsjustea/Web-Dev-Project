28bcd9afd9c60984ef786cea5bf8e679a9583142
BryanTohWS <bryantws1999@gmail.com>
2025-02-04 20:24:38 +0100

    Reverting to base flask server for testing:

--- Lab2/server.py
+++ Lab2/server.py
@@ -8,67 +8,67 @@ app = Flask(__name__)
 
 
 @app.route("/")  # Initial landing page of the user.
-def initPage():
-    return render_template("client.html")
+def hello_world():
+    return "Hello world"
 
 
-# Defining all the necessary functions from serverstub
-@app.route("/user/sign_in", methods=["POST"])
-def sign_in():
-    return (
-        jsonify({"success": False, "message": "Sign In Successful"}),
-        500,
-    )  # Placeholder
+# # Defining all the necessary functions from serverstub
+# @app.route("/user/sign_in", methods=["POST"])
+# def sign_in():
+#     return (
+#         jsonify({"success": False, "message": "Sign In Successful"}),
+#         500,
+#     )  # Placeholder
 
 
-@app.route("/user/sign_up", methods=["POST"])
-def sign_in():
-    return (
-        jsonify({"success": True, "message": "Sign Up Successful"}),
-        500,
-    )  # Placeholder
+# @app.route("/user/sign_up", methods=["POST"])
+# def sign_in():
+#     return (
+#         jsonify({"success": True, "message": "Sign Up Successful"}),
+#         500,
+#     )  # Placeholder
 
 
-@app.route("/user/sign_out", methods=["POST"])
-def sign_out():
-    return (
-        jsonify({"success": True, "message": "Sign Out Successful"}),
-        500,
-    )  # Placeholder
+# @app.route("/user/sign_out", methods=["POST"])
+# def sign_out():
+#     return (
+#         jsonify({"success": True, "message": "Sign Out Successful"}),
+#         500,
+#     )  # Placeholder
 
 
-@app.route("/users/change_password", methods=["POST"])
-def change_password():
-    return (jsonify({"success": True, "message": "Password Changed Successfully"}), 500)
+# @app.route("/users/change_password", methods=["POST"])
+# def change_password():
+#     return (jsonify({"success": True, "message": "Password Changed Successfully"}), 500)
 
 
-@app.route("/user/get_user_data_by_email", methods=["GET"])
-def get_user_data_by_email():
-    return (
-        jsonify({"success": True, "message": "User Data Retrieved Successfully"}),
-        500,
-    )
+# @app.route("/user/get_user_data_by_email", methods=["GET"])
+# def get_user_data_by_email():
+#     return (
+#         jsonify({"success": True, "message": "User Data Retrieved Successfully"}),
+#         500,
+#     )
 
 
-@app.route("/user/get_user_data_by_token", methods=["GET"])
-def get_user_data_by_token():
-    return (
-        jsonify({"success": True, "message": "User Data Retrieved Successfully"}),
-        500,
-    )
+# @app.route("/user/get_user_data_by_token", methods=["GET"])
+# def get_user_data_by_token():
+#     return (
+#         jsonify({"success": True, "message": "User Data Retrieved Successfully"}),
+#         500,
+#     )
 
 
-@app.route("/user/get_user_messages_by_email", methods=["GET"])
-def get_user_messages_by_email():
-    return (
-        jsonify({"success": True, "message": "User Messages Retrieved Successfully"}),
-        500,
-    )
+# @app.route("/user/get_user_messages_by_email", methods=["GET"])
+# def get_user_messages_by_email():
+#     return (
+#         jsonify({"success": True, "message": "User Messages Retrieved Successfully"}),
+#         500,
+#     )
 
 
-@app.route("/messages/post_message", methods=["POST"])
-def post_message():
-    return (jsonify({"success": True, "message": "Message Posted Successfully"}), 500)
+# @app.route("/messages/post_message", methods=["POST"])
+# def post_message():
+#     return (jsonify({"success": True, "message": "Message Posted Successfully"}), 500)
 
 
 if __name__ == "__main__":
