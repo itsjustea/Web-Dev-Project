51bf62d8a7aaba4b72d724426be47b0316754472
BryanTohWS <bryantws1999@gmail.com>
2025-02-04 20:21:32 +0100

    FEAT: Flask Server Initial Setup

---
+++ Lab2/database_helper.py
@@ -0,0 +1,5 @@
+# This file will contain all the functions that access and control the database and shall contain some SQL scripts.
+# This file will be used by the server.py to access the database. This file shall NOT contain any domain functions
+# like sign in or sign up and shall only contain data-centric functionality like find_user(), remove_user(),
+# create_post() and etc. Implementing sign_in() in server.py shall involve a call to find_user() implemented in
+# database_helper.py .
